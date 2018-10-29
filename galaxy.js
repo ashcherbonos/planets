f7d278d39cf27dc97bf6ce7325cb9cb587383bd8
alexLitslink <oleksandr.shcherbonos@litslink.com>
2018-10-29 19:20:55 +0200

    code refactored

--- galaxy.js
+++ galaxy.js
@@ -14,6 +14,30 @@ class Planet {
         this.element=document.getElementById(id);
         this.phase=phase;
     }
+
+    MoveByRim(rim){
+        let phase=this.SumPhases(rim.currentPhase,this.phase);
+        if(phase>VISIBLE_SECTOR_OF_GALAXY_END){
+            this.phase-=VISIBLE_SECTOR_OF_GALAXY;
+            phase=this.SumPhases(rim.currentPhase,this.phase);
+        }else if(phase<VISIBLE_SECTOR_OF_GALAXY_START){
+            this.phase+=VISIBLE_SECTOR_OF_GALAXY;
+            phase=this.SumPhases(rim.currentPhase,this.phase);
+        }
+        let top=(rim.orbit.r*Math.cos(phase)+rim.orbit.y);
+        let left=(rim.orbit.r*Math.sin(phase)+rim.orbit.x);
+        this.element.style.top=top+'%';
+        this.element.style.left=left+'%';
+        this.Scale(top);
+    }
+
+    SumPhases(rimPhase,planetPhase){
+        return planetPhase+rimPhase;
+    }
+
+    Scale(proximity){
+        this.element.style.transform="scale("+(PLANET_MIN_SCALE+PLANET_MAX_DELTA_SCALE*proximity/100)+")";
+    }
 }
 
 export class Rim{
@@ -21,7 +45,7 @@ export class Rim{
         this.speed=speed;        
         this.run=true;
         this.currentPhase=0;
-        this.planets=Rim.InitPlanets(planetIds);
+        this.planets=this.InitPlanets(planetIds);
         this.lastFramePhase=0;
         this.inertiaSpeed=0;
         this.orbit=orbit;
@@ -30,7 +54,7 @@ export class Rim{
         this.currentFramePhase=0;
     }
 
-    static InitPlanets(planetIds){
+    InitPlanets(planetIds){
         let planetsPhase=0;
         let deltaPhase=VISIBLE_SECTOR_OF_GALAXY/planetIds.length ;
         let planets=[];
@@ -40,6 +64,16 @@ export class Rim{
         });
         return planets;
     }
+
+    MoveBy(angle){
+        this.currentPhase+=angle;
+        this.planets.forEach(planet=>planet.MoveByRim(this));
+    }
+
+    MoveTo(angle){
+        this.currentPhase=angle;
+        this.planets.forEach(planet=>planet.MoveByRim(this));
+    }
 }
 
 export class Galaxy{
@@ -47,6 +81,7 @@ export class Galaxy{
         this.galaxy=rims;
         this.lastFrameTime=0;
         this.currentFrameTime=0;
+        this.AddListeners();
     }
 
     AddListeners(){
@@ -55,12 +90,12 @@ export class Galaxy{
                 rim.run=false;
                 rim.inertiaSpeed=0;
                 let mousePosition={x:event.clientX,y:event.clientY};
-                rim.startPhase=rim.currentPhase+this.GetAngle(mousePosition,rim.div);
+                rim.startPhase=rim.currentPhase+Utils.GetAngle(mousePosition,rim.div);
             };
 
             rim.div.onmousemove=(event)=>{
                 let mousePosition={x:event.clientX,y:event.clientY};
-                this.MoveRimTo(rim.startPhase-this.GetAngle(mousePosition,rim.div),rim);
+                rim.MoveTo(rim.startPhase-Utils.GetAngle(mousePosition,rim.div));
                 rim.lastFramePhase=rim.currentFramePhase;
                 rim.currentFramePhase=rim.currentPhase;            
                 this.lastFrameTime=this.currentFrameTime;
@@ -71,74 +106,41 @@ export class Galaxy{
                 this.galaxy.forEach(rim=>rim.run=true);
                 let deltaTime=Math.max((this.currentFrameTime-this.lastFrameTime),FRAME_TIME);
                 let inertia=(rim.currentPhase-rim.lastFramePhase)/deltaTime;
-                inertia=this.Clamp(inertia,MAX_SPEED);
+                inertia=Utils.Clamp(inertia,MAX_SPEED);
                 rim.inertiaSpeed=inertia;
             };
         });
     }
 
-    MoveRimBy(angle,rim){
-        rim.currentPhase+=angle;
-        rim.planets.forEach(planet=>this.MovePlanet(planet,rim));
+    Run(){
+        setInterval(()=>{
+            this.galaxy.forEach(rim=>{
+                if(rim.run){
+                    rim.inertiaSpeed*=DECCELERATION;
+                    let speed=BASE_SPEED_PER_FRAME*rim.speed+rim.inertiaSpeed;
+                    rim.MoveBy(speed);
+                }
+            });
+        }, FRAME_TIME);
+    }
+} 
+
+class Utils{
+    static  Clamp(num,border){
+        return Math.min(Math.max(num,-border),border);
     }
 
-    MoveRimTo(angle,rim){
-        rim.currentPhase=angle;
-        rim.planets.forEach(planet=>this.MovePlanet(planet,rim));
-    }
-
-    MovePlanet(planet,rim){
-        let phase=this.SumPhases(rim.currentPhase,planet.phase);
-        if(phase>VISIBLE_SECTOR_OF_GALAXY_END){
-            planet.phase-=VISIBLE_SECTOR_OF_GALAXY;
-            phase=this.SumPhases(rim.currentPhase,planet.phase);
-        }else if(phase<VISIBLE_SECTOR_OF_GALAXY_START){
-            planet.phase+=VISIBLE_SECTOR_OF_GALAXY;
-            phase=this.SumPhases(rim.currentPhase,planet.phase);
-        }
-        let top=(rim.orbit.r*Math.cos(phase)+rim.orbit.y);
-        let left=(rim.orbit.r*Math.sin(phase)+rim.orbit.x);
-        planet.element.style.top=top+'%';
-        planet.element.style.left=left+'%';
-        this.ScalePlanet(planet,top);
-    }
-
-    SumPhases(planetPhase,rimPhase){
-        return planetPhase+rimPhase;
-    }
-
-    ScalePlanet(planet,proximity){
-        planet.element.style.transform="scale("+(PLANET_MIN_SCALE+PLANET_MAX_DELTA_SCALE*proximity/100)+")";
-    }
-
-    GetAngle(point,div){
+    static  GetAngle(point,div){
         let center=this.GetCenterOf(div);
         let dx=point.x-center.x;
         let dy=point.y-center.y;
         return Math.atan2(dy,dx);
     }
 
-    GetCenterOf(div){
+    static GetCenterOf(div){
         return{
             x:div.offsetLeft+div.offsetWidth*0.6,
             y:div.offsetTop+div.offsetHeight*0.5
         }
     }
-
-    Clamp(num,border){
-        return Math.min(Math.max(num,-border),border);
-    }
-
-    Run(){
-        this.AddListeners();
-        setInterval(()=>{
-            this.galaxy.forEach(rim=>{
-                if(rim.run){
-                    rim.inertiaSpeed*=DECCELERATION;
-                    let speed=BASE_SPEED_PER_FRAME*rim.speed+rim.inertiaSpeed;
-                    this.MoveRimBy(speed,rim);
-                }
-            });
-        }, FRAME_TIME);
-    }
-} 
+}
